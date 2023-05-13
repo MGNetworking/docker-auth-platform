f976a36bdc6c37ac140bd354637f4fcb72ddf124
Maxime <ghalem.maxime@gmail.com>
2023-05-13 11:45:10 +0200

    add dossier : postgres_home/backups

--- db-save.py
+++ db-save.py
@@ -10,7 +10,7 @@ DB_PASSWORD = "NzOgZsn29PewtEyQECEE"
 DB_NAME = "kc_db"
 
 # Configuration des paramètres de la sauvegarde
-BACKUP_DIR = "./data-save"
+BACKUP_DIR = "./postgres_home/backups"
 DATE_FORMAT = "%Y-%m-%d_%H-%M-%S"
 
 # Génération du nom de fichier pour la sauvegarde
@@ -19,6 +19,7 @@ backup_filename = f"{DB_NAME}_backup_{timestamp}.sql"
 
 # Commande de sauvegarde de la base de données avec Exécution de la commande de sauvegarde
 result = subprocess.run(f"docker exec -t postgres-db pg_dump -U {DB_USER} -d {DB_NAME} -c > {BACKUP_DIR}/{backup_filename}", shell=True)
+
 
 # Affichage d'un message de confirmation
 print(f"La sauvegarde de la base de données {DB_NAME} a été créée avec succès.")
--- restore.py
+++ restore.py
@@ -5,11 +5,16 @@ DB_PASSWORD = "NzOgZsn29PewtEyQECEE"
 DB_HOST = "localhost"
 DB_PORT = "5400"
 DB_NAME = "kc_db"
-BACKUP_DIR = "./data-save"
-backup_filename = "teste.sql"
+BACKUP_DIR = "./postgres_home/backups"
+backup_filename = "kc_db_backup_2023-05-13_10-40-25.sql"
 
 # construction de la commande de restauration
-restore_cmd = f"pg_restore --dbname=postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME} {os.path.join(BACKUP_DIR, backup_filename)}"
+restore_cmd = f"pg_restore --dbname=postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME} \"{os.path.join(BACKUP_DIR, backup_filename)}\""
+print(restore_cmd)
+
+teste= "pg_restore --dbname=postgresql://max_admin:NzOgZsn29PewtEyQECEE@localhost:5400/kc_db ./postgres_home/backups/kc_db_backup_2023-05-13_10-40-25.sql"
+print(teste)
+#os.system(f"docker exec -it postgres-db bash -c '{teste}'")
 
 # exécution de la commande de restauration dans le conteneur Docker
 os.system(f"docker exec -it postgres-db bash -c '{restore_cmd}'")
