343d2402b28b41dd0b5a3c429720dbceb279976f
Maxime VM desktop <ghalem.maxime@gmail.com>
2023-05-27 17:02:11 +0200

    gestion des backups base de données kc_db et schema kc_sh de cette méme db via les fichier : db-save_kc.py et restore_kc.py à jour et fonctionnel

--- db-save_kc.py
+++ db-save_kc.py
@@ -5,34 +5,57 @@ import os
 # Configuration des paramètres de la base de données
 DB_HOST = "localhost"
 DB_PORT = "5400"
-DB_USER = "kc_db"
+DB_USER = "max_admin"
 DB_PASSWORD = "NzOgZsn29PewtEyQECEE"
-DB_NAME = "max_admin"
+DB_NAME = "kc_db"
+DB_SCHEMA="kc_sh"
 
-# Configuration des paramètres de la sauvegarde
-BACKUP_DIR_host = "./postgres_home/backups"
+# accès un fichier depuis extérieur du conteneur
+BACKUP_DB = "./postgres_home/backups/kc_db"
+
 # Path dans le conteneur
-BACKUP_DIR = "/var/backups"
+BACKUP_DIR_DB = "/var/backups/kc_db"
+BACKUP_DIR_SCHEMA = "/docker-entrypoint-initdb.d"
 DATE_FORMAT = "%Y-%m-%d_%H-%M-%S"
 
-# Génération du nom de fichier pour la sauvegarde
+# Génération des nom de fichier pour les sauvegardes
 timestamp = datetime.datetime.now().strftime(DATE_FORMAT)
-backup_filename = f"{DB_NAME}_backup_{timestamp}.dump"
-compressed_filename = f"{DB_NAME}_backup_{timestamp}.tar.gz"
+backup_filename_DB = f"{DB_NAME}_backup_{timestamp}.dump"
+compressed_filename_DB = f"{DB_NAME}_backup_{timestamp}.tar.gz"
+backup_filename_SCHEMA = f"{DB_SCHEMA}_backup.sql.bak"
 
 # Commande de sauvegarde de la base de données avec Exécution de la commande de sauvegarde
-backup_command = f"docker exec -t postgres-db pg_dump -U {DB_USER} -d {DB_NAME} -F c --clean -f {BACKUP_DIR}/{backup_filename}"
-print("Lancement de la sauvegarde ...")
+backup_command = f"docker exec -t postgres-db pg_dump -U {DB_USER} -d {DB_NAME} -F c --clean -f {BACKUP_DIR_DB}/{backup_filename_DB}"
+print("1. Lancement de la sauveagde de la base de données kc_db avec processus de nettoyage des objets")
 subprocess.run(backup_command, shell=True)
 
 # Compression du fichier de sauvegarde
-print("Compression du fichier de sauvegarde")
-compress_command = f"tar -czvf {BACKUP_DIR_host}/{compressed_filename} -C {BACKUP_DIR_host} {backup_filename}"
+print("2. Compression du fichier de sauvegarde (.tar.gz)")
+compress_command = f"tar -czvf {BACKUP_DB}/{compressed_filename_DB} -C {BACKUP_DB} {backup_filename_DB}"
 subprocess.run(compress_command, shell=True)
 
 # Suppression du fichier de sauvegarde non compressé
-print("Suppression du fichier de sauvegarde non compressé")
-os.remove(f"{BACKUP_DIR_host}/{backup_filename}")
+print("3. Suppression du fichier de sauvegarde non compressé (.dump)")
+os.remove(f"{BACKUP_DB}/{backup_filename_DB}")
 
 # Affichage d'un message de confirmation
 print(f"La sauvegarde de la base de données {DB_NAME} a été créée avec succès et compressée.")
+
+# Commande de sauvegarde de la base de données avec Exécution de la commande de sauvegarde
+# backup_command = f"docker exec -t postgres-db pg_dump -U {DB_USER} -d {DB_NAME} -n {DB_SCHEMA} -F p -f {BACKUP_SCHEMA}/{backup_filename_SCHEMA}"
+backup_command = f"docker exec -t postgres-db pg_dump -U {DB_USER} -d {DB_NAME} -n {DB_SCHEMA} -F p -f {BACKUP_DIR_SCHEMA}/{backup_filename_SCHEMA}"
+print("4. Lancement de la sauvegarde du schema kc_sh dans la base de données kc_db")
+
+try:
+    subprocess.run(backup_command, shell=True, check=True)
+    # Affichage d'un message de confirmation
+    print(f"La sauvegarde du schema {DB_SCHEMA} a été créée avec succès ")
+
+except subprocess.CalledProcessError as e:
+    print("Message : "+str(e))
+
+    if "No such file or directory" in str(e):
+        print("Une erreur s'est produite lors de la sauvegarde : fichier de sortie inexistant.")
+
+    else:
+        print("Une autre erreur s'est produite lors de la sauvegarde.")
