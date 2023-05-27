98362802730340b090dbc90bd5137c06a9cdfcac
Maxime VM desktop <ghalem.maxime@gmail.com>
2023-05-27 17:05:41 +0200

    ajout des fichiers d'initialilsations pour la BD kc_db et le schema kc_sh à jour et focntionnel

--- postgres_home/init/00_base_kc.sql
+++ postgres_home/init/00_base_kc.sql
@@ -8,9 +8,9 @@ CREATE DATABASE kc_db
   WITH OWNER = max_admin
       ENCODING = 'UTF8';
 
--- connection a base de données ghoverblog
+-- Connection a base de données ghoverblog
 \c kc_db
 
 -- Restauration de la sauvegarde SQL
--- Ce fichier contient la sauvegarde du schema kc_sh
+-- Ce fichier contient un Backup du schema kc_sh
 \i /docker-entrypoint-initdb.d/kc_sh_backup.sql.bak
