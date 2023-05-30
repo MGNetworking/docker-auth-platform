87779ff1f2735de1f5d83ec3ccd5fe12ca8e4a7f
Maxime VM desktop <ghalem.maxime@gmail.com>
2023-05-30 16:03:27 +0200

    modification des fichiers db_save_blog et db_save_blog

--- db-save_blog.py
+++ db-save_blog.py
@@ -6,17 +6,19 @@ import datetime
 import os
 import logging
 
+##########################################
+# Gestion des variables
 # Configuration des paramètres de la base de données
 DB_HOST = "localhost"
 DB_PORT = "5400"
 DB_USER = "max_admin"
 DB_PASSWORD = "NzOgZsn29PewtEyQECEE"
 DB_NAME = "ghoverblog"
-DB_SCHEMA="ms_article"
+DB_SCHEMA = "ms_article"
 
-# Path dans le conteneur
+# chemin d'accès au fichier depuis l'intérieur du conteneur
 BACKUP_DIR_SCHEMA = "/docker-entrypoint-initdb.d"
-#DATE_FORMAT = "%Y-%m-%d_%H-%M-%S"
+# DATE_FORMAT = "%Y-%m-%d_%H-%M-%S"
 DATE_FORMAT = "%d-%m-%Y_%H:%M:%S"
 
 ##########################################
@@ -43,11 +45,11 @@ BACKUP_DB_LOG = f"{BACKUP_DB}/logfile.log"
 # Création du fichier de log vide s'il n'existe pas
 log_file = os.path.join(BACKUP_DB, "logfile.log")
 if not os.path.exists(log_file):
-    open(log_file, "w").close()
-
+	open(log_file, "w").close()
 
 # Configuration du logger
-logging.basicConfig(filename=BACKUP_DB_LOG , filemode='a', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(filename=BACKUP_DB_LOG, filemode='a', level=logging.INFO,
+					format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Utilisation du logger pour les messages de log
 logger = logging.getLogger()
@@ -57,69 +59,128 @@ print(f"SAUVEGARDE DB : DEBUT #################")
 print(f"1er parti Backup Data Base {DB_NAME}")
 logger.info(f"1er parti Bachup Data Base {DB_NAME}")
 
-
 # Vérification et création du dossier si nécessaire
-if not os.path.exists(BACKUP_DB):
-    os.makedirs(BACKUP_DB)
-    print(f"1. Dossier : {BACKUP_DB} à été créé avec succès.")
-    logger.info(f"1. Dossier : {BACKUP_DB} à été créé avec succès.")
-
 if os.path.exists(BACKUP_DB):
-    print(f"1. Dossier : {BACKUP_DB} est présent")
-    logger.info(f"1. Dossier : {BACKUP_DB} est présent")
+	print(f"1. Dossier : {BACKUP_DB} est présent")
+	logger.info(f"1. Dossier : {BACKUP_DB} est présent")
+else:
+	os.makedirs(BACKUP_DB)
+	print(f"1. Dossier : {BACKUP_DB} à été créé avec succès.")
+	logger.info(f"1. Dossier : {BACKUP_DB}  à été  créé avec succès.")
 
 # Génération du nom de fichier pour la sauvegarde
 timestamp = datetime.datetime.now().strftime(DATE_FORMAT)
-backup_filename = f"{DB_NAME}_backup_{timestamp}.dump"
-compressed_filename = f"{DB_NAME}_backup_{timestamp}.tar.gz"
+backup_filename_DB = f"{DB_NAME}_backup_{timestamp}.dump"
+compressed_filename_DB = f"{DB_NAME}_backup_{timestamp}.tar.gz"
 backup_filename_SCHEMA = f"{DB_SCHEMA}_backup.sql.bak"
 
-# Commande de sauvegarde de la base de données avec clean des anciennnes tables
-backup_command = f"docker exec -t postgres-db pg_dump -U {DB_USER} -d {DB_NAME} -F c --clean -f {BACKUP_DIR_DB}/{backup_filename}"
-print(f"1. Lancement de la sauvegarde de la base de données {DB_SCHEMA} avec processus de nettoyage des objets")
-logger.info(f"1. Lancement de la sauvegarde de la base de données {DB_SCHEMA} avec processus de nettoyage des objets")
-subprocess.run(backup_command, shell=True)
-
-# Compression du fichier de sauvegarde
-print("2. Compression du fichier de sauvegarde (.tar.gz)")
-logger.info("2. Compression du fichier de sauvegarde (.tar.gz)")
-compress_command = f"tar -czvf {BACKUP_DB}/{compressed_filename} -C {BACKUP_DB} {backup_filename}"
-subprocess.run(compress_command, shell=True)
-
-# Suppression du fichier de sauvegarde non compressé
-print("3. Suppression du fichier de sauvegarde non compressé")
-logger.info("3. Suppression du fichier de sauvegarde non compressé")
-os.remove(f"{BACKUP_DB}/{backup_filename}")
-
-# Affichage d'un message de confirmation
-print(f"La sauvegarde de la base de données {DB_NAME} a été créée avec succès et compressée.")
-logger.info(f"La sauvegarde de la base de données {DB_NAME} a été créée avec succès et compressée.")
-
-
-# Commande de sauvegarde de la base de données avec Exécution de la commande de sauvegarde
-# backup_command = f"docker exec -t postgres-db pg_dump -U {DB_USER} -d {DB_NAME} -n {DB_SCHEMA} -F p -f {BACKUP_SCHEMA}/{backup_filename_SCHEMA}"
-backup_command = f"docker exec -t postgres-db pg_dump -U {DB_USER} -d {DB_NAME} -n {DB_SCHEMA} -F p -f {BACKUP_DIR_SCHEMA}/{backup_filename_SCHEMA}"
-print(f"4. Lancement de la sauvegarde du schema {DB_SCHEMA} dans la base de données kc_db")
-logger.info(f"4. Lancement de la sauvegarde du schema {DB_SCHEMA} dans la base de données kc_db")
-
 try:
-    subprocess.run(backup_command, shell=True, check=True)
-    # Affichage d'un message de confirmation
-    print(f"La sauvegarde du schema {DB_SCHEMA} a été créée avec succès ")
-    logger.info(f"La sauvegarde du schema {DB_SCHEMA} a été créée avec succès ")
+	# Commande de sauvegarde de la base de données avec clean des anciennnes tables
+	backup_command = f"docker exec -t postgres-db pg_dump -U {DB_USER} -d {DB_NAME} -F c --clean -f {BACKUP_DIR_DB}/{backup_filename_DB}"
+	print(f"2. Lancement de la sauvegarde de la base de données {DB_SCHEMA} avec processus de nettoyage des objets")
+	logger.info(
+		f"2. Lancement de la sauvegarde de la base de données {DB_SCHEMA} avec processus de nettoyage des objets")
+	result = subprocess.run(backup_command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+	standard_output = result.stdout.decode("utf-8")
+	error_output = result.stderr.decode("utf-8")
+	if error_output:
+		print("Une erreur a était générer pendant la création du dump ")
+		print(error_output)
+		logger.error("Une erreur a était générer pendant la création du dump ")
+		logger.error(error_output)
+	else:
+		print(f"Le dump était réaliser avec succès")
+		logger.info("Le dump était réaliser avec succès")
+		logger.info(f"PATH : {BACKUP_DB}/{backup_filename_DB}")
+
+	# Compression du fichier de sauvegarde
+	compress_command = f"tar -czvf {BACKUP_DB}/{compressed_filename_DB} -C {BACKUP_DB} {backup_filename_DB}"
+	print("3. Compression du fichier de sauvegarde (.tar.gz)")
+	logger.info("3. Compression du fichier de sauvegarde (.tar.gz)")
+	result = subprocess.run(compress_command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+	error_output = result.stderr.decode("utf-8")
+	standard_output = result.stdout.decode("utf-8")
+	if error_output:
+		print("Une erreur a était générer pendant la création de l'archive ")
+		print(error_output)
+		logger.error("Une erreur a était générer pendant la création de l'archive  ")
+		logger.error(error_output)
+	else:
+		print("l'archive à été réaliser avec succès")
+		logger.info("l'archive était réaliser avec succès")
+		logger.info(f"PATH : {BACKUP_DB}/{compressed_filename_DB}")
+
+	# Suppression du fichier de sauvegarde non compressé
+	print("4. Suppression du fichier de sauvegarde non compressé")
+	logger.info("4. Suppression du fichier de sauvegarde non compressé")
+	extracedFile = f"{BACKUP_DB}/{backup_filename_DB}"
+
+	# Vérification de la suppression
+	if os.path.exists(extracedFile):
+		os.remove(extracedFile)
+		logger.info(f"le dossier : {extracedFile} ")
+	else:
+		logger.error(f"Dossier : {extracedFile} n'existe pas ")
+
+	# Affichage d'un message de confirmation
+	print(f"La sauvegarde de la base de données {DB_NAME} a été créée avec succès et compressée.")
+	logger.info(f"La sauvegarde de la base de données {DB_NAME} a été créée avec succès et compressée.")
+
 
 except subprocess.CalledProcessError as e:
-    print("Message : "+str(e))
-    logger.error(f"Message : {e}")
+	print(f"Message : {e}")
+	logger.error(f"Message : {e}")
 
-    if "No such file or directory" in str(e):
-        print("Une erreur s'est produite lors de la sauvegarde : fichier de sortie inexistant.")
-        logger.error("Une erreur s'est produite lors de la sauvegarde : fichier de sortie inexistant.")
+	if "No such file or directory" in str(e):
+		print("Une erreur s'est produite lors de la sauvegarde : fichier de sortie inexistant.")
+		logger.error("Une erreur s'est produite lors de la sauvegarde : fichier de sortie inexistant.")
 
-    else:
-        print("Une autre erreur s'est produite lors de la sauvegarde.")
-        logger.error("Une autre erreur s'est produite lors de la sauvegarde.")
+	else:
+		print("Une autre erreur s'est produite lors de la sauvegarde.")
+		logger.error("Une autre erreur s'est produite lors de la sauvegarde.")
 
+try:
 
-logger.info(f"SAUVEGARDE DB : FIN #################")
-print(f"SAUVEGARDE DB : FIN #################")
+	logger.info("")
+	print("")
+	print(f"2eme parti Backup Schema : {DB_SCHEMA}")
+	logger.info(f"2eme parti Backup Schema : {DB_SCHEMA}")
+
+	# Commande de sauvegarde de la base de données avec Exécution de la commande de sauvegarde
+	backup_command = f"docker exec -t postgres-db pg_dump -U {DB_USER} -d {DB_NAME} -n {DB_SCHEMA} -F p -f {BACKUP_DIR_SCHEMA}/{backup_filename_SCHEMA}"
+	print(f"4. Lancement de la sauvegarde du schema {DB_SCHEMA} dans la base de données kc_db")
+	logger.info(f"4. Lancement de la sauvegarde du schema {DB_SCHEMA} dans la base de données kc_db")
+
+	result = subprocess.run(backup_command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	# Affichage d'un message de confirmation
+	print(f"La sauvegarde du schema {DB_SCHEMA} a été créée avec succès ")
+	logger.info(f"La sauvegarde du schema {DB_SCHEMA} a été créée avec succès ")
+
+	error_output = result.stderr.decode("utf-8")
+	standard_output = result.stdout.decode("utf-8")
+
+	if error_output:
+		print("Une erreur a était générer pendant la création du backup SQL")
+		print(error_output)
+		logger.error("Une erreur a était générer pendant la création du backup SQL")
+		logger.error(error_output)
+	else:
+		print("Le backup SQL à été réaliser avec succès")
+		logger.info(f"Le backup SQL à été réaliser avec succès PATH : {BACKUP_DIR_SCHEMA}/{backup_filename_SCHEMA}")
+
+except subprocess.CalledProcessError as e:
+	print(f"Message : {e}")
+	logger.error(f"Message : {e}")
+
+	if "No such file or directory" in str(e):
+		print("Une erreur s'est produite lors de la sauvegarde : fichier de sortie inexistant.")
+		logger.error("Une erreur s'est produite lors de la sauvegarde : fichier de sortie inexistant.")
+
+	else:
+		print("Une autre erreur s'est produite lors de la sauvegarde.")
+		logger.error("Une autre erreur s'est produite lors de la sauvegarde.")
+
+logger.info(f"SAUVEGARDE DB :  {DB_SCHEMA} FIN #################")
+print(f"SAUVEGARDE DB : {DB_SCHEMA} FIN #################")
--- db-save_kc.py
+++ db-save_kc.py
@@ -6,8 +6,7 @@ import datetime
 import os
 import logging
 
-
-###############################
+##########################################
 # Gestion des variables
 # Configuration des paramètres de la base de données
 DB_HOST = "localhost"
@@ -15,7 +14,7 @@ DB_PORT = "5400"
 DB_USER = "max_admin"
 DB_PASSWORD = "NzOgZsn29PewtEyQECEE"
 DB_NAME = "kc_db"
-DB_SCHEMA="kc_sh"
+DB_SCHEMA = "kc_sh"
 
 # chemin d'accès au fichier depuis l'intérieur du conteneur
 BACKUP_DIR_SCHEMA = "/docker-entrypoint-initdb.d"
@@ -31,25 +30,25 @@ mois_en_cours = maintenant.month
 
 # Création du chemin d'accès au fichier pour la création du bachup DB
 # Créer en fonction du mois en cours, accès depuis extérieur du conteneur
-BACKUP_DB = f"./postgres_home/backups/kc_db/{mois_en_cours}_{annee_en_cours}"
+DIR_BACKUP = f"./postgres_home/backups/kc_db/{mois_en_cours}_{annee_en_cours}"
 
 # Créer en fonction du mois en cours, accès depuis intérieur du conteneur
-BACKUP_DIR_DB = f"/var/backups/kc_db/{mois_en_cours}_{annee_en_cours}"
+DIR_BACKUP_CONTENEUR = f"/var/backups/kc_db/{mois_en_cours}_{annee_en_cours}"
 
 # Création du répertoire des logs s'il n'existe pas
-os.makedirs(BACKUP_DB, exist_ok=True)
+os.makedirs(DIR_BACKUP, exist_ok=True)
 
 # Création de chemin du dossier pour les log
-BACKUP_DB_LOG = f"{BACKUP_DB}/logfile.log"
+BACKUP_DB_LOG = f"{DIR_BACKUP}/logfile.log"
 
 # Création du fichier de log vide s'il n'existe pas
-log_file = os.path.join(BACKUP_DB, "logfile.log")
+log_file = os.path.join(DIR_BACKUP, "logfile.log")
 if not os.path.exists(log_file):
-    open(log_file, "w").close()
-
+	open(log_file, "w").close()
 
 # Configuration du logger
-logging.basicConfig(filename=BACKUP_DB_LOG , filemode='a', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(filename=BACKUP_DB_LOG, filemode='a', level=logging.INFO,
+					format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Utilisation du logger pour les messages de log
 logger = logging.getLogger()
@@ -60,14 +59,13 @@ print(f"1er parti Backup Data Base {DB_NAME}")
 logger.info(f"1er parti Backup Data Base {DB_NAME}")
 
 # Vérification et création du dossier si nécessaire
-if not os.path.exists(BACKUP_DB):
-    os.makedirs(BACKUP_DB)
-    print(f"1. Dossier : {BACKUP_DB} à été créé avec succès.")
-    logger.info(f"1. Dossier : {BACKUP_DB}  à été  créé avec succès.")
-
-if os.path.exists(BACKUP_DB):
-    print(f"1. Dossier : {BACKUP_DB} est présent")
-    logger.info(f"1. Dossier : {BACKUP_DB} est présent")
+if os.path.exists(DIR_BACKUP):
+	print(f"1. Dossier : {DIR_BACKUP} est présent")
+	logger.info(f"1. Dossier : {DIR_BACKUP} est présent")
+else:
+	os.makedirs(DIR_BACKUP)
+	print(f"1. Dossier : {DIR_BACKUP} à été créé avec succès.")
+	logger.info(f"1. Dossier : {DIR_BACKUP}  à été  créé avec succès.")
 
 # Génération des nom de fichier pour les sauvegardes
 timestamp = datetime.datetime.now().strftime(DATE_FORMAT)
@@ -77,91 +75,105 @@ backup_filename_SCHEMA = f"{DB_SCHEMA}_backup.sql.bak"
 
 try:
 
+	# Commande de sauvegarde de la base de données avec Exécution de la commande de sauvegarde
+	backup_command = f"docker exec -t postgres-db pg_dump -U {DB_USER} -d {DB_NAME} -F c --clean -f {DIR_BACKUP_CONTENEUR}/{backup_filename_DB}"
+	print(f"2. Lancement de la sauvegarde de la base de données {DB_NAME} avec processus de nettoyage des objets")
+	logger.info(f"2. Lancement de la sauvegarde de la base de données {DB_NAME} avec processus de nettoyage des objets")
+	result = subprocess.run(backup_command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    # Commande de sauvegarde de la base de données avec Exécution de la commande de sauvegarde
-    backup_command = f"docker exec -t postgres-db pg_dump -U {DB_USER} -d {DB_NAME} -F c --clean -f {BACKUP_DIR_DB}/{backup_filename_DB}"
-    print(f"2. Lancement de la sauvegarde de la base de données {DB_NAME} avec processus de nettoyage des objets")
-    logger.info(f"2. Lancement de la sauvegarde de la base de données {DB_NAME} avec processus de nettoyage des objets")
-    result = subprocess.run(backup_command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	standard_output1 = result.stdout.decode("utf-8")
+	error_output1 = result.stderr.decode("utf-8")
+	if error_output1:
+		print("Une erreur a était générer pendant la création du dump ")
+		print(error_output1)
+		logger.error("Une erreur a était générer pendant la création du dump ")
+		logger.error(error_output1)
+	else:
+		print(f"Le dump était réaliser avec succès")
+		logger.info("Le dump était réaliser avec succès")
+		logger.info(f"PATH : {DIR_BACKUP}/{backup_filename_DB}")
 
-    standard_output1 = result.stdout.decode("utf-8")
-    error_output1 = result.stderr.decode("utf-8")
-    if error_output1:
-        print("Une erreur a était générer pendant la création du dump ")
-        print(error_output1)
-        logger.error("Une erreur a était générer pendant la création du dump ")
-        logger.error(error_output1)
-    else:
-        print(f"Le dump était réaliser avec succès")
-        logger.info("Le dump était réaliser avec succès")
-        logger.info(f"PATH : {BACKUP_DB}/{backup_filename_DB}")
+	# Compression du fichier de sauvegarde depuis l'extérieur du conteneur
+	compress_command = f"tar -czvf {DIR_BACKUP}/{compressed_filename_DB} -C {DIR_BACKUP} {backup_filename_DB}"
+	print("3. Compression du fichier de sauvegarde (.tar.gz)")
+	logger.info("3. Compression du fichier de sauvegarde (.tar.gz)")
+	result = subprocess.run(compress_command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
+	error_output = result.stderr.decode("utf-8")
+	standard_output = result.stdout.decode("utf-8")
+	if error_output:
+		print("Une erreur a était générer pendant la création de l'archive ")
+		print(error_output)
+		logger.error("Une erreur a était générer pendant la création de l'archive  ")
+		logger.error(error_output)
+	else:
+		print("l'archive à été réaliser avec succès")
+		logger.info("l'archive était réaliser avec succès")
+		logger.info(f"PATH : {DIR_BACKUP}/{compressed_filename_DB}")
 
-    # Compression du fichier de sauvegarde depuis l'extérieur du conteneur
-    compress_command = f"tar -czvf {BACKUP_DB}/{compressed_filename_DB} -C {BACKUP_DB} {backup_filename_DB}"
-    print("3. Compression du fichier de sauvegarde (.tar.gz)")
-    result = subprocess.run(compress_command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	# Suppression du fichier de sauvegarde non compressé
+	print("4. Suppression du fichier de sauvegarde non compressé (.dump)")
+	logger.info("4. Suppression du fichier de sauvegarde non compressé (.dump)")
+	extracedFile = f"{DIR_BACKUP}/{backup_filename_DB}"
 
-    error_output2 = result.stderr.decode("utf-8")
-    standard_output2 = result.stdout.decode("utf-8")
-    if error_output2:
-        print("Une erreur a était générer pendant la création de l'archive ")
-        print(error_output2)
-        logger.error("Une erreur a était générer pendant la création de l'archive  ")
-        logger.error(error_output2)
-    else:
-        print("l'archive à été réaliser avec succès")
-        logger.info("l'archive était réaliser avec succès")
-        logger.info(f"PATH : {BACKUP_DB}/{compressed_filename_DB}")
+	# Vérification de la suppression
+	if os.path.exists(extracedFile):
+		os.remove(extracedFile)
+		logger.info(f"le dossier : {extracedFile} ")
+	else:
+		logger.error(f"Dossier : {extracedFile} n'existe pas ")
 
-
-    # Suppression du fichier de sauvegarde non compressé
-    print("3. Suppression du fichier de sauvegarde non compressé (.dump)")
-    logger.info("3. Suppression du fichier de sauvegarde non compressé (.dump)")
-    os.remove(f"{BACKUP_DB}/{backup_filename_DB}")
-    logger.info("Fichier .dump supprimer ")
-
-    # Affichage d'un message de confirmation
-    print(f"La sauvegarde de la base de données {DB_NAME} dans un fichier compressée a été créée avec succès")
-    logger.info(f"La sauvegarde de la base de données {DB_NAME} dans un fichier compressée a été créée avec succès")
+	# Affichage d'un message de confirmation
+	print(f"La sauvegarde de la base de données {DB_NAME} dans un fichier compressée a été créée avec succès")
+	logger.info(f"La sauvegarde de la base de données {DB_NAME} dans un fichier compressée a été créée avec succès")
 
 except subprocess.CalledProcessError as e:
-    print(f"Une erreur s'est produite lors d'une commande  : {e}" )
-    print(f"Code de sortie : {e.returncode}")
+	print(f"Une erreur s'est produite lors d'une commande  : {e}")
+	print(f"Code de sortie : {e.returncode}")
 
-    logger.error(f"Une erreur s'est produite lors d'une commande  : {e}" )
-    logger.error(f"Code de sortie : {e.returncode}")
-
-
+	logger.error(f"Une erreur s'est produite lors d'une commande  : {e}")
+	logger.error(f"Code de sortie : {e.returncode}")
 
 try:
 
-    logger.info("")
-    print("")
-    print(f"2eme parti Backup Schema : {DB_SCHEMA}")
-    logger.info(f"2eme parti Backup Schema : {DB_SCHEMA}")
+	logger.info("")
+	print("")
+	print(f"2eme parti Backup Schema : {DB_SCHEMA}")
+	logger.info(f"2eme parti Backup Schema : {DB_SCHEMA}")
 
-    # Commande de sauvegarde de la base de données avec Exécution de la commande de sauvegarde
-    backup_command = f"docker exec -t postgres-db pg_dump -U {DB_USER} -d {DB_NAME} -n {DB_SCHEMA} -F p -f {BACKUP_DIR_SCHEMA}/{backup_filename_SCHEMA}"
-    print(f"1. Lancement de la sauvegarde du schema {DB_SCHEMA} dans la base de données kc_db")
-    logger.info(f"1. Lancement de la sauvegarde du schema {DB_SCHEMA} dans la base de données kc_db")
+	# Commande de sauvegarde de la base de données avec Exécution de la commande de sauvegarde
+	backup_command = f"docker exec -t postgres-db pg_dump -U {DB_USER} -d {DB_NAME} -n {DB_SCHEMA} -F p -f {BACKUP_DIR_SCHEMA}/{backup_filename_SCHEMA}"
+	print(f"1. Lancement de la sauvegarde du schema {DB_SCHEMA} dans la base de données kc_db")
+	logger.info(f"1. Lancement de la sauvegarde du schema {DB_SCHEMA} dans la base de données kc_db")
 
-    subprocess.run(backup_command, shell=True, check=True)
-    # Affichage d'un message de confirmation
-    print(f"La sauvegarde du schema {DB_SCHEMA} a été créée avec succès ")
-    logger.info(f"La sauvegarde du schema {DB_SCHEMA} a été créée avec succès")
+	result = subprocess.run(backup_command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	# Affichage d'un message de confirmation
+	print(f"La sauvegarde du schema {DB_SCHEMA} a été créée avec succès ")
+	logger.info(f"La sauvegarde du schema {DB_SCHEMA} a été créée avec succès")
+
+	error_output = result.stderr.decode("utf-8")
+	standard_output = result.stdout.decode("utf-8")
+
+	if error_output:
+		print("Une erreur a était générer pendant la création du backup SQL")
+		print(error_output)
+		logger.error("Une erreur a était générer pendant la création du backup SQL")
+		logger.error(error_output)
+	else:
+		print("Le backup SQL à été réaliser avec succès")
+		logger.info(f"Le backup SQL à été réaliser avec succès PATH : {BACKUP_DIR_SCHEMA}/{backup_filename_SCHEMA}")
 
 except subprocess.CalledProcessError as e:
-    print("Message : "+str(e))
-    logger.error(f"Message : {e}")
+	print(f"Message : {e}")
+	logger.error(f"Message : {e}")
 
-    if "No such file or directory" in str(e):
-        print("Une erreur s'est produite lors de la sauvegarde : fichier de sortie inexistant.")
-        logger.error("Une erreur s'est produite lors de la sauvegarde : fichier de sortie inexistant.")
+	if "No such file or directory" in str(e):
+		print("Une erreur s'est produite lors de la sauvegarde : fichier de sortie inexistant.")
+		logger.error("Une erreur s'est produite lors de la sauvegarde : fichier de sortie inexistant.")
 
-    else:
-        print("Une autre erreur s'est produite lors de la sauvegarde.")
-        logger.error("Une autre erreur s'est produite lors de la sauvegarde.")
+	else:
+		print("Une autre erreur s'est produite lors de la sauvegarde.")
+		logger.error("Une autre erreur s'est produite lors de la sauvegarde.")
 
-logger.info(f"SAUVEGARDE DB : FIN ###################")
-print(f"SAUVEGARDE DB : FIN ###################")
+logger.info(f"SAUVEGARDE DB :  {DB_SCHEMA} FIN #################")
+print(f"SAUVEGARDE DB : {DB_SCHEMA} FIN #################")
