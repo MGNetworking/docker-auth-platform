aacccd2e54d6c868ac1ee61375963e4d01fc37b5
Maxime <ghalem.maxime@gmail.com>
2024-03-14 16:08:15 +0100

    modification

--- down-dev.ps1
+++
@@ -1,5 +0,0 @@
-# Stop et supprime le serveur
-docker-compose -f docker-compose-dev.yml  down
-
-# liste des images
-docker images
--- run-dev.ps1
+++
@@ -1,8 +0,0 @@
-# construction des images puis création des conteneur Docker en mode détaché et enregistrement des logs
-
-# l'option --build remplace l'ancienne images
-docker-compose -f docker-compose-dev.yml up -d --build
-
-# voir les logs
-docker-compose -f docker-compose-dev.yml logs -f
-
